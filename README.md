ca0537c124c20733f0536d38384504854073d099
Christian <christian.straubhaar@gmail.com>
2016-07-02 16:18:57 -0500

    got album view mostly done, working on pic view

--- README.md
+++ README.md
@@ -8,6 +8,11 @@
 3. Connect everything and test it's connections (log something to console from data.js and add some css to the app div)
 4. some basic SCSS styles, set up bourbon and neat (since we'll want neat to make all the spacing tidy and easy)
 5. Start writing the javascript and jquery to manifest the start page
+6. finished the basics for the first page, started the interior of an album (all wrapped inside an `.on('click')` to render the pictures page)
+7. fiddled with different ways of formatting, first trying 'neat', then running into some issues (see issue 1) and then trying 'masonry' (see issue 2), ended up returning to 'neat'.
+8. finished building album view and the basics of the album view sidebar, then worked on building picture view (inside another `.on('click')`)
 
 ## Issues
-1. having issues with neat in SASS (`@include omega(4n)`) because when I hid the albums I still had them in the same container as the pictures, so the albums would still count as children and throw off the omega count. Jess suggested I put them in entirely different containers, which containers I would then hide or show. Makes more semantic sense, too.
+1. having issues with neat in SASS (tried `@include omega(4n)` on all tiles and then later an `if(i%4===0)` statement, no luck, both still always adding `.omega` to the first or second pic, not the fourth) because when I hid the albums I still had them in the same container as the pictures, so the albums would still count as children and throw off the omega count. Jess suggested I put them in entirely different containers, which containers I would then hide or show. Makes more semantic sense, too.
+2. A quick search unearthed a javascript library called Masonry (http://masonry.desandro.com/) which might also be a much better fit than neat for making tiled layouts. He has another handy library called 'imagesLoaded' to make sure that all the images are fully loaded before running the layout, which keeps it from breaking. I'm trying including both.
+  - Having issues getting all the Masonry methods to work right. Suddenly all the images are cascading down the page really weirdly. Maybe not worth it after all. Reverting to neat again.
--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,27 @@
-var $main = $('.container');
+var $albumMain = $('.album-container');
+var $picMain = $('.pic-container');
+var $picList = $('.picture-list');
+
 var $header = $('.heading');
+
 var tileHTML = '<li><img src=""/><h4 class="label"></h4></li>';
 var gapHTML = '<div class="gap"></div>';
 var buttonHTML = '<input type="button" class="album-button"/>';
+var pictureHTML = '<div class="picture"><img src=""/><h6 class="caption"></h6></div>';
+var $body = $('body');
+
+// var $grid = $('.grid').masonry({
+//   columnWidth: 10,
+//   itemSelector: '.grid-item',
+//
+// });
 
 
 $(document).ready(function() {
 
+  // $grid.imagesLoaded().progress( function() {
+  //   $grid.masonry('layout');
+  // });
 
 data.forEach(function(album, ia, arr) {
 
@@ -17,33 +32,62 @@ data.forEach(function(album, ia, arr) {
   $albumThumb.children('h4').text(album.albumName);
 
   $albumThumb.addClass('album');
-  $albumThumb.addClass('tile');
-  $main.append($albumThumb);
+  $albumThumb.addClass('grid-item');
+  // $albumThumb.addClass('tile');
+  $albumMain.append($albumThumb);
 
   if (((ia+1)%4)===0 && ia !== 0) {
     $albumThumb.addClass('omega');
   }
 
 
-  $albumThumb.on('click', function functionName() {
-
+  $albumThumb.on('click', function () {
     $header.text(album.albumName);
-    $main.children('.album').addClass('hide');
-    $main.children('.album').removeClass('tile');
+    $albumMain.addClass('hide');
+    $albumMain.removeClass('show');
+
+
+    // $albumMain.children('.album').removeClass('tile');
 
     album.albumPics.forEach(function(picture, ib) {
+
+      $picMain.addClass('show');
+      $picMain.removeClass('hide');
+
 
       var $picThumb = $(tileHTML);
       $picThumb.children('img').attr('src', picture.path);
       $picThumb.children('img').addClass('thumbnail');
-      $picThumb.children('h4').text(picture.caption);
-      $picThumb.addClass('pic');
-      $picThumb.addClass('tile');
-      $main.append($picThumb);
+      $picThumb.children('h4').text(picture.picName);
 
-      if (ib%4===0) {
+      $picThumb.addClass('pic');
+      $picThumb.addClass('show');
+      $picThumb.addClass('grid-item');
+      // $picThumb.addClass('tile');
+
+      $picList.append( $picThumb);
+
+      if (((ib+1)%3)===0 && ib !== 0) {
         $picThumb.addClass('omega');
+      } else if ((ib%3)===0 && ib !== 0) {
+        $picThumb.addClass('row-first');
       }
+
+      $picThumb.on('click', function() {
+
+        $header.text(picture.picName);
+        $picMain.addClass('hide');
+        $picMain.removeClass('show');
+
+        var $picture = $('pictureHTML');
+        $picture.children('img').attr('src', picture.path);
+        $picture.children('img').addClass('thumbnail');
+
+      });
+
+      // if (ib%4===0) {
+      //   $picThumb.addClass('omega');
+      // }
 
     });
 
@@ -53,8 +97,15 @@ data.forEach(function(album, ia, arr) {
       $albumButton.attr('value', album.albumName);
       $sidebar.append($albumButton);
 
+      $sidebar.addClass('sidebar');
+
+      $picMain.append($sidebar);
+
     });
-    $main.append($sidebar);
+
+
+
+
   });
 
 
